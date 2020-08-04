c0ea27fc57b0414f789bbb17683541095007e416
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2020-08-04 12:30:05 +0530

    used string resources to display simple messages

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtView2 = findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
+
+        Log.i("LifeCycle", "onCreate() invoked");
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.i("LifeCycle", "onStart() invoked");
+    }
+    public void onRestart() {
+        super.onRestart();
+
+        Log.i("LifeCycle", "onRestart() invoked");
+    }
+    public void onResume() {
+        super.onResume();
+    }
+}
